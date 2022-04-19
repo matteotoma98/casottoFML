5056a4339de7079d72cec15d894edc9d78c3e64d
matteotoma98 <matteotoma98@hotmail.it>
2022-04-19 17:20:15 +0200

    Crezione Progetto su Intellij e prova connessione al db con MySQL

---
+++ casotto/src/main/java/com/unicam/cs/ids/casotto/CasottoApplication.java
@@ -0,0 +1,30 @@
+package com.unicam.cs.ids.casotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@SpringBootApplication
+public class CasottoApplication {
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/casotto", "root", "casottofml");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from people");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstname"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Probabilmente non sei riuscito a connetterti al database. Assicurati di avere mysql installato e che tu sia collegato al database.");
+        }
+
+        SpringApplication.run(CasottoApplication.class, args);
+
+    }
+}
---
+++ casotto/src/test/java/com/unicam/cs/ids/casotto/CasottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicam.cs.ids.casotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasottoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
